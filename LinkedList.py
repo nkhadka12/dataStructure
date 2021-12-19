c82b3a167456a6aafbf689b7ca28406b81b86169
Nischal Khadka <82045084+nkhadka12@users.noreply.github.com>
2021-12-19 01:59:48 -0600

    "This is my code for linkedList"

--- LinkedList.py
+++ LinkedList.py
@@ -1,2 +1,48 @@
-print("Hello")
-print("This is me Nischal")
+# Name: Nischal Khadka
+# Email: nischalkhadka.nk@gmail.com
+# purpose: To learn linked list, and its common use
+
+class Node:
+    def __init__(self,value,next):
+        self.value = value
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def insert_at_begining(self,value):
+        node = Node(value,self.head)
+        self.head = node
+
+    def insert_at_end(self,value):
+        if self.head == None:
+            node = Node(value,self.head)
+            self.head = node
+        else:
+            itr = self.head
+            while itr.next != None:
+                itr = itr.next
+
+            node = Node(value,itr.next)
+            itr.next = node
+    def print_node(self):
+        itr = self.head
+        while itr != None:
+            print(itr.value)
+            itr = itr.next
+
+linkedList = LinkedList()
+linkedList.insert_at_begining("Nischal")
+linkedList.insert_at_begining("Sagun")
+linkedList.insert_at_begining("Nishant")
+linkedList.insert_at_begining("Keshav")
+linkedList.insert_at_begining("Sarita")
+
+
+
+linkedList.insert_at_end("Manamaya")
+linkedList.insert_at_end("Baa")
+linkedList.insert_at_end("Khadga")
+
+linkedList.print_node()
